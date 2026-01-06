65d0e5979a13afc1379aab23368cffaa21a76b3e
Leonel <166152300+leoneloficial@users.noreply.github.com>
2026-01-06 03:47:11 -0400

    Actualizar info-menu.js

--- comandos/info-menu.js
+++ comandos/info-menu.js
@@ -351,10 +351,10 @@ let handler = async (m, { conn }) => {
                   })
                 },
                 {
-                //  name: 'cta_url',
-                 // buttonParamsJson: //JSON.stringify({
-                    //display_text: //'𝗢𝗽𝗲𝗻 𝗦𝗼𝘂𝗿𝗰𝗲',
-                   // url: 'https://github.com/Ado21/WaMeowBot'
+                  name: 'cta_url',
+                  buttonParamsJson: JSON.stringify({
+                    display_text: '𝗢𝗽𝗲𝗻 𝗦𝗼𝘂𝗿𝗰𝗲',
+                    url: ''
                   })
                 }
               ]
