1f14fc95288b17beedfabb9b0897baacf205f2fe
Leonel <166152300+leoneloficial@users.noreply.github.com>
2026-01-06 04:12:02 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   nombrebot: 'sumi sakurasawa',
   moneda: '$ᴅᴏʟᴀʀᴇs',
-  apikey: 'leonel', // Pon tu apikey aqui, consiguela en: https://api-adonix.ultraplus.click
+  apikey: '', // Pon tu apikey aqui, consiguela en: https://api-adonix.ultraplus.click
   prefijo: '.',
 
   owner: [
